4befa1edb9c249712f1d26ebad97cfce922bb4c1
Alfred <arista21dominguez@gmail.com>
2025-04-20 16:50:37 -0600

    init para back

---
+++ back-top-shop/src/main/java/com/back_top_shop/BackTopShopApplication.java
@@ -0,0 +1,13 @@
+package com.back_top_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTopShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTopShopApplication.class, args);
+	}
+
+}
